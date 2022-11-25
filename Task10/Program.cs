49674eabf0e70b1e993a7b57961f3dbdd0b6a9db
a.gazalov <gazalov@list.ru>
2022-11-25 12:54:21 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int x = int.Parse(Console.ReadLine());
+x = (x/10) % 10;
+Console.WriteLine($"Вторая цифра введенного числа: {x}.");
